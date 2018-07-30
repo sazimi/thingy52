8abbec56ce2bd58f023005842ef0664fec214623
Sherry <sa@epgs.com>
2018-07-30 15:36:37 +0200

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'th-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'slider-thingy52';
+}
